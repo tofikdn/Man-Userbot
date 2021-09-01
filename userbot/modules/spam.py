6152568f6902c05534d3fb016678de8b4eb65f1a
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-09-01 22:00:15 +0700

    Update spam.py

--- userbot/modules/spam.py
+++ userbot/modules/spam.py
@@ -10,69 +10,104 @@ from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^\\.cspam (.+)")
-async def tmeme(e):
-    cspam = str(e.pattern_match.group(1))
+@register(outgoing=True, pattern=r"^\.cspam (.+)")
+async def leter_spam(cspammer):
+    cspam = str(cspammer.pattern_match.group(1))
     message = cspam.replace(" ", "")
-    await e.delete()
+    await cspammer.delete()
     for letter in message:
-        await e.respond(letter)
+        await cspammer.respond(letter)
     if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#CSPAM\n" "TSpam was executed successfully"
+        await cspammer.client.send_message(
+            BOTLOG_CHATID, "**#CSPAM** was executed successfully"
         )
 
 
 @register(outgoing=True, pattern=r"^\.wspam (.+)")
-async def t_meme(e):
-    wspam = str(e.pattern_match.group(1))
+async def word_spam(wspammer):
+    wspam = str(wspammer.pattern_match.group(1))
     message = wspam.split()
-    await e.delete()
+    await wspammer.delete()
     for word in message:
-        await e.respond(word)
+        await wspammer.respond(word)
     if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#WSPAM\n" "WSpam was executed successfully"
+        await wspammer.client.send_message(
+            BOTLOG_CHATID, "**#WSPAM** was executed successfully"
         )
 
 
 @register(outgoing=True, pattern=r"^\.spam (\d+) (.+)")
-async def spammer(e):
-    counter = int(e.pattern_match.group(1))
-    spam_message = str(e.pattern_match.group(2))
-    await e.delete()
-    await asyncio.wait([e.respond(spam_message) for i in range(counter)])
-    if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#SPAM\n" "Spam was executed successfully"
-        )
+async def spammer(spamm):
+    try:
+        counter = int(spamm.pattern_match.group(1).split(" ", 1)[0])
+    except IndexError:
+        await spamm.edit("**Gunakan Perintah** `.spam <jumlah spam> <text>` **atau** `.spam <jumlah spam>` **sambil reply pesan**")
+        await sleep(5)
+        await spamm.delete()
+        return
+    textx = await spamm.get_reply_message()
+    if not textx:
+        try:
+            spam_message = str(spamm.pattern_match.group(1).split(" ", 1)[1])
+        except IndexError:
+            await spamm.edit("**Gunakan Perintah** `.spam <jumlah spam> <text>` **atau** `.spam <jumlah spam>` **sambil reply pesan**")
+            await sleep(5)
+            await spamm.delete()
+            return
+        await spamm.delete()
+        await asyncio.wait([spamm.respond(spam_message) for i in range(counter)])
+        if BOTLOG:
+            await spamm.client.send_message(
+                BOTLOG_CHATID, "**#SPAM** was executed successfully"
+            )
+    elif (textx and textx.text):
+        await spamm.delete()
+        await asyncio.wait([spamm.respond(textx) for i in range(counter)])
+        if BOTLOG:
+            await spamm.client.send_message(
+                BOTLOG_CHATID, "**#SPAM** was executed successfully\n"
+            )
 
 
 @register(outgoing=True, pattern=r"^\.picspam (\d+) (.+)")
-async def tiny_pic_spam(e):
-    counter = int(e.pattern_match.group(1))
-    link = str(e.pattern_match.group(2))
-    await e.delete()
+async def tiny_pic_spam(pspam):
+    message = pspam.text
+    text = message.split()
+    try:
+        counter = int(text[1])
+        link = str(text[2])
+    except IndexError:
+        await pspam.edit("**Gunakan Perintah** `.picspam` <jumlah spam> <link image/gif>")
+        await sleep(5)
+        await pspam.delete()
+        return
+    await pspam.delete()
     for _ in range(1, counter):
-        await e.client.send_file(e.chat_id, link)
+        await pspam.client.send_file(pspam.chat_id, link)
     if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#PICSPAM\n" "PicSpam was executed successfully"
+        await pspam.client.send_message(
+            BOTLOG_CHATID, "**#PICSPAM** was executed successfully"
         )
 
 
 @register(outgoing=True, pattern="^.delayspam (.*)")
-async def spammer(e):
-    spamDelay = float(e.pattern_match.group(1).split(" ", 2)[0])
-    counter = int(e.pattern_match.group(1).split(" ", 2)[1])
-    spam_message = str(e.pattern_match.group(1).split(" ", 2)[2])
-    await e.delete()
+async def dspammer(dspam):
+    try:
+        spamDelay = float(dspam.pattern_match.group(1).split(" ", 2)[0])
+        counter = int(dspam.pattern_match.group(1).split(" ", 2)[1])
+        spam_message = str(dspam.pattern_match.group(1).split(" ", 2)[2])
+    except IndexError:
+        await dspam.edit("**Gunakan Perintah** `.delayspam` <detik> <jumlah spam> <text>")
+        await sleep(5)
+        await dspam.delete()
+        return
+    await dspam.delete()
     for _ in range(1, counter):
-        await e.respond(spam_message)
+        await dspam.respond(spam_message)
         await sleep(spamDelay)
     if BOTLOG:
-        await e.client.send_message(
-            BOTLOG_CHATID, "#DelaySPAM\n" "DelaySpam was executed successfully"
+        await dspam.client.send_message(
+            BOTLOG_CHATID, "**#DelaySPAM** was executed successfully"
         )
 
 
