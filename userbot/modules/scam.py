39c350d0be18851046c9e1553521e7d1fad9dd01
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-09-07 15:40:09 +0700

    Create scam.py

---
+++ userbot/modules/scam.py
@@ -0,0 +1,50 @@
+import asyncio
+from random import choice, randint
+
+from telethon.errors import BadRequestError
+from telethon.tl.functions.channels import EditAdminRequest
+from telethon.tl.types import ChatAdminRights
+
+from userbot.utils import edit_delete
+from userbot.events import register
+from userbot import ALIVE_NAME
+
+
+@register(outgoing=True, pattern=r"^\.scam(?:\s|$)([\s\S]*)")
+async def _(event):
+    options = [
+        "typing",
+        "contact",
+        "game",
+        "location",
+        "voice",
+        "round",
+        "video",
+        "photo",
+        "document",
+    ]
+    input_str = event.pattern_match.group(1)
+    args = input_str.split()
+    if len(args) == 0:
+        scam_action = choice(options)
+        scam_time = randint(300, 360)
+    elif len(args) == 1:
+        try:
+            scam_action = str(args[0]).lower()
+            scam_time = randint(300, 360)
+        except ValueError:
+            scam_action = choice(options)
+            scam_time = int(args[0])
+    elif len(args) == 2:
+        scam_action = str(args[0]).lower()
+        scam_time = int(args[1])
+    else:
+        await edit_delete(event, "**Invalid Syntax !!**")
+        return
+    try:
+        if scam_time > 0:
+            await event.delete()
+            async with event.client.action(event.chat_id, scam_action):
+                await asyncio.sleep(scam_time)
+    except BaseException:
+        return
